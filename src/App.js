2f0a15789aaaee444fbbca0b0c43abfa18c3cbf7
mude-me <mude-me@gmail.com>
2021-11-05 14:58:05 -0300

    chore: add rule eslint pattern code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React, { useState, useEffect, memo } from 'react';
+import mockData from './services/api';
+import ListItem from './components/TreeView';
+import setListed from './helpers/setListed';
+
 import './styles/global.css';
 
 function App() {
+  const [listedEdit, setListedEdit] = useState([]);
+  const [peoples, setPeoples] = useState([]);
+
+  useEffect(() => {
+    const data = localStorage.getItem('dados');
+
+    if (data) {
+      setListedEdit(JSON.parse(data));
+    } else {
+      setListedEdit(setListed);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('dados', JSON.stringify(listedEdit));
+  }, [listedEdit]);
+
+  useEffect(() => {
+    setPeoples(mockData);
+  }, []);
+
   return (
-    <div className="App">
-      <p>Felipe Weiduschadt de Carvalho</p>
-    </div>
+    <>
+      {peoples &&
+        Object.values(peoples).map((i) => {
+          return (
+            <ListItem listEdit={listedEdit} setListedEdit={setListedEdit} item={i} key={i.id} />
+          );
+        })}
+    </>
   );
 }
 
-export default App;
+export default memo(App);
